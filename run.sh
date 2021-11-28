eecd29d94ca368004c9aa679185722226a1c65c1
Marcelle <marcelleb@bbd.co.za>
2021-11-28 13:15:37 +0200

    path fix

--- run.sh
+++ run.sh
@@ -3,6 +3,6 @@
 x=1
 while [ $x -le 1000 ]
 do
-    docker run -t -d -p 100$x:10000 -v /extenstions:/extentions --restart unless-stopped xpra-chrome
+    docker run -t -d -p 100$x:10000 -v ~/extenstions:/extentions --restart unless-stopped xpra-chrome
     x=$(( $x + 1 ))
 done
