cea6d3b9f6663b6306a1e4847f45fe3630619c61
Marcelle <marcelleb@bbd.co.za>
2022-09-15 17:42:29 +0200

    changed url

--- install_docker_and_pull_image.sh
+++ install_docker_and_pull_image.sh
@@ -1,5 +1,5 @@
 curl -fsSL https://get.docker.com -o get-docker.sh
 sudo sh get-docker.sh
 docker pull ghcr.io/marcellebond/docker_chrome
-curl -fsSL "https://downgit.github.io/#/home?url=https://github.com/MarcelleBond/Docker_Chrome/blob/master/run.sh" -o run.sh
+wget -L "https://raw.githubusercontent.com/MarcelleBond/Docker_Chrome/master/run.sh"
 sudo sh run.sh
