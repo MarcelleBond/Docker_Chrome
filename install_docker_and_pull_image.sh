9e7ed96921220f1d11d3b015ed78438cc6045a55
Marcelle <marcelleb@bbd.co.za>
2022-09-15 17:26:50 +0200

    install script for docker

---
+++ install_docker_and_pull_image.sh
@@ -0,0 +1,5 @@
+curl -fsSL https://get.docker.com -o get-docker.sh
+sudo sh get-docker.sh
+docker pull ghcr.io/marcellebond/docker_chrome
+curl -fsSL "https://downgit.github.io/#/home?url=https://github.com/MarcelleBond/Docker_Chrome/blob/master/run.sh" -o run.sh
+sudo sh run.sh
