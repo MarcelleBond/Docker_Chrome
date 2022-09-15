b5b7165b45be01b6a147f9fa0239afdd1f762b73
Marcelle <marcelleb@bbd.co.za>
2022-09-15 17:22:31 +0200

    changed image name

--- run.sh
+++ run.sh
@@ -3,6 +3,6 @@
 x=1
 while [ $x -le 50 ]
 do
-    docker run -t -d -p 100$x:10000 -v ~/extenstions:/extentions --restart unless-stopped xpra-chrome
+    docker run -t -d -p 100$x:10000 --restart unless-stopped docker_chrome
     x=$(( $x + 1 ))
 done
