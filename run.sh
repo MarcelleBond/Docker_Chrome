bcc9a7028aefa376d4124adc85295b623d8e2734
Marcelle <marcelleb@bbd.co.za>
2022-10-13 16:37:30 +0200

    added port number

--- run.sh
+++ run.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 
 x=1
+port=5000
 while [ $x -le $1 ]
 do
-    sudo docker run -t -d -p 100$x:10000 --restart unless-stopped hotbox.azurecr.io/marcellebond/docker_chrome:1.0.1
+    sudo docker run -t -d -p $port:10000 --restart unless-stopped hotbox.azurecr.io/marcellebond/docker_chrome:1.0.1
     x=$(( $x + 1 ))
+    port=$(( $port + 1 ))
 done
